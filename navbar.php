56283686d558efdd095c864b9e8e971e795adf5e
Mary Catherine Scott <maryscott@u.boisestate.edu>
2016-09-20 13:10:50 -0600

    adding basic nav bar

---
+++ navbar.php
@@ -0,0 +1,15 @@
+<?php
+echo '<h1>Spindles Cookbook</h1>
+
+<button>LOG IN</button>
+
+<div>
+	<ul>
+		<li>dashboard</li>
+		<li>search</li>
+		<li>browse</li>
+		<li>add a recipe</li>
+		<li>account</li>
+		<li>suggestions</li>
+	</ul>
+</div>'
