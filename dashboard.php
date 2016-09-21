fcb823e7e42fb89859c392cf730e93ee456a38c3
Mary Catherine Scott <maryscott@u.boisestate.edu>
2016-09-20 20:52:17 -0600

    adding css and basic pages

---
+++ dashboard.php
@@ -0,0 +1,13 @@
+<html>
+<div>
+	<?php
+		include "navbar.php";
+	?>
+</div>
+
+
+
+
+
+</html>
+
--- navbar.php
+++ navbar.php
@@ -5,12 +5,12 @@ echo '<h1>Spindles Cookbook</h1>
 
 <div>
 	<ul>
-		<li>dashboard</li>
-		<li>search</li>
-		<li>browse</li>
-		<li>add a recipe</li>
-		<li>account</li>
-		<li>suggestions</li>
+		<li><a href="dashbord.php">dashboard</a></li>
+		<li><a href="search.php">search</a></li>
+		<li><a href="browse.php">browse</a></li>
+		<li><a href="add_a_recipe.php">add a recipe</a></li>
+		<li><a href="account.php">account</a></li>
+		<li><a href="suggestions.php">suggestions</a></li>
 	</ul>
 </div>'
 
