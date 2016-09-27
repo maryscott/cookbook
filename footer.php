269905fdf34b19fa44fefaa8851985b1b51bf3ae
Mary Catherine Scott <maryscott@u.boisestate.edu>
2016-09-26 21:33:04 -0600

    trying to fix

--- footer.php
+++ footer.php
@@ -2,10 +2,10 @@
 
 echo '
 	<ul id="footerList">
-		<li><a href=#here>Dashboard</a></li>
-		<li><a href=#there>search</a></li>
-		<li><a href=#anywhere>add a recipe</a></li>
-		<li><a href=#somewhere>contact us</a></li>
+		<li><a href="pages/dashboard.php">Dashboard</a></li>
+		<li><a href="pages/search.php">search</a></li>
+		<li><a href="pages/addarecipe.php">add a recipe</a></li>
+		<li><a href="pages/dashboard.php">contact us</a></li>
 	</ul>
 '
 
