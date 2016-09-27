122bc6e5ff784eaed4100b43cc9123fc7158f936
Mary Catherine Scott <maryscott@u.boisestate.edu>
2016-09-26 22:59:03 -0600

    moving button

--- navbar.php
+++ navbar.php
@@ -1,11 +1,12 @@
 <?php
-echo "<h1>Spindles Cookbook</h1>";
-
-echo "<button>LOG IN</button>";
+echo '<div>
+<h1>Spindles Cookbook</h1>
+<button>LOG IN</button>
+	</div>'
 
 
 echo '<ul>
-		<li><a href="dashbord.php">dashboard</a></li>
+		<li><a href="dashboard.php">dashboard</a></li>
 		<li><a href="search.php">search</a></li>
 		<li><a href="browse.php">browse</a></li>
 		<li><a href="add_a_recipe.php">add a recipe</a></li>
