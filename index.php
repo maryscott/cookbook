89e473a07a79f9ee2f95c1c91420a0826fcf3894
Mary Catherine Scott <maryscott@u.boisestate.edu>
2016-09-26 22:47:44 -0600

    bigger pic

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 		</div>
 
 	<div id="content">
-		<img src="FrozenCupcake1.jpg" alt="cupcake" style="float:left;width:200px">
+		<img src="FrozenCupcake1.jpg" alt="cupcake" style="float:left;width:300px">
 		<p>	Biscuit biscuit croissant cake tiramisu sweet. Gummi bears jujubes carrot cke wafter pastry wafer.  Cake apple pie chocolate sugar plum tiramisu seame snaps halvah halvahwafer.  Lemon drops cotton candy ice cream halvah chocolate cake gummi bears halvah.  Souffle jelly-o biscuit dessert lemon drops.  wafer caramels donut.  Dragee gingerbread chocolate cake jelly-o croissant biscuit cake.  Danish candy icing cake apple pie tootsie roll icing.  Caramels tiramisu cotton candy wafer chocolate gummi bears chocolate.  Chocolate bar dessert pudding candy canes caramels dragee.  Chupa chups wafer chocolate bar.  Tiramisu marzipan halvah lemon drops.  Cookie cupcake tiramisu.  Lemon drops jujubes candy jelly beans.
 		</p>
 	</div>
