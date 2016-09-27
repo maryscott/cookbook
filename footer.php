6eb5436212f28d7d22a793d64694b774e97cbbe8
Mary Catherine Scott <maryscott@u.boisestate.edu>
2016-09-26 21:34:13 -0600

    fixed links

--- footer.php
+++ footer.php
@@ -2,10 +2,10 @@
 
 echo '
 	<ul id="footerList">
-		<li><a href="pages/dashboard.php">Dashboard</a></li>
-		<li><a href="pages/search.php">search</a></li>
-		<li><a href="pages/addarecipe.php">add a recipe</a></li>
-		<li><a href="pages/dashboard.php">contact us</a></li>
+		<li><a href="dashboard.php">Dashboard</a></li>
+		<li><a href="search.php">search</a></li>
+		<li><a href="addarecipe.php">add a recipe</a></li>
+		<li><a href="dashboard.php">contact us</a></li>
 	</ul>
 '
 
