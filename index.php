43b73ad1214fff104bd6d891655ce275a18e9986
Mary Catherine Scott <maryscott@u.boisestate.edu>
2016-09-26 21:48:21 -0600

    taking things out

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 
 <div id="content">
 Hello World! This is a test to see if the auto awesomeness is working or not.
-<p><img src="FrozenCupcake1.jpg" alt="cupcake" style="float:left;width:120px">
+<p>
 	Biscuit biscuit croissant cake tiramisu sweet. Gummi bears jujubes carrot cke wafter pastry wafer.  Cake apple pie chocolate sugar plum tiramisu seame snaps halvah halvahwafer.  Lemon drops cotton candy ice cream halvah chocolate cake gummi bears halvah.  Souffle jelly-o biscuit dessert lemon drops.  wafer caramels donut.  Dragee gingerbread chocolate cake jelly-o croissant biscuit cake.  Danish candy icing cake apple pie tootsie roll icing.  Caramels tiramisu cotton candy wafer chocolate gummi bears chocolate.  Chocolate bar dessert pudding candy canes caramels dragee.  Chupa chups wafer chocolate bar.  Tiramisu marzipan halvah lemon drops.  Cookie cupcake tiramisu.  Lemon drops jujubes candy jelly beans.
 </p>
 </div>
