e27b59ba6ca7078670106d6efa5d3d21cd6c0951
Patrick <Patrick>
2016-10-04 21:04:52 -0600

    color changes, fix in favicon for all pages added contact us page

--- contact_us.php
+++ contact_us.php
@@ -11,7 +11,10 @@
 	</div>
 
 	<div id="content" class="clearfix">
-
+		<h3>Contact Us</h3>
+		<p id="contactInfo">
+			If you have any questions, comments, concerns, snarky remarks, or suggestions to improve the site you can contact the owner at this e-mail address:  maryscott@u.boisestate.edu
+		</p>
 	</div>
 
 	<div id="footer">
--- navbar.php
+++ navbar.php
@@ -9,12 +9,12 @@ echo '<div id="title" class="clearfix">
 
 	<div>
 		<ul>
-			<li><a href="dashboard.php">dashboard</a></li>
-			<li><a href="search.php">search</a></li>
-			<li><a href="browse.php">browse</a></li>
-			<li><a href="add_a_recipe.php">add a recipe</a></li>
-			<li><a href="account.php">account</a></li>
-			<li><a href="suggestions.php">suggestions</a></li>
+			<li><a href="dashboard.php">Dashboard</a></li>
+			<li><a href="search.php">Search</a></li>
+			<li><a href="browse.php">Browse</a></li>
+			<li><a href="add_a_recipe.php">Add a Recipe</a></li>
+			<li><a href="account.php">My Account</a></li>
+			<li><a href="suggestions.php">Suggestions</a></li>
 		</ul>
 	</div>
 ';
